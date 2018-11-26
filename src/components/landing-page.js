f1c317c2de6ed256e3412fb6d41e069dfbb86bfc
Joe-Pena <joepena@protonmail.com>
2018-11-26 10:16:25 -0700

    Add description to application landing page

--- src/components/landing-page.js
+++ src/components/landing-page.js
@@ -12,7 +12,12 @@ export function LandingPage(props) {
 
     return (
         <div className="home">
-            <h2>Javthon</h2>
+            <h2>Welcome to Javthon!</h2>
+            <p>
+                Here, you will be learning about all the different languages <br />
+                there are at a very brief level, so that when you see them <br /> 
+                out in the wild, you will know what language you're looking at!
+            </p>
             <LoginForm />
             <Link to="/register">Sign Up</Link>
         </div>
