986a9d26cc0c58e8e728f4cbac2ac9d35a5876b7
Joe-Pena <joepena@protonmail.com>
2019-01-07 18:54:38 -0700

    Add new background to dashboard

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -4,6 +4,118 @@ import {connect} from 'react-redux';
 import requiresLogin from './requires-login';
 // import {fetchProtectedData} from '../actions/protected-data';
 import { fetchQuestion, answeredQuestion, getProgress, fetchStats } from '../actions/questions';
+import Particles from 'react-particles-js';
+
+const particleOptions = {
+    "particles": {
+      "number": {
+        "value": 154,
+        "density": {
+          "enable": true,
+          "value_area": 800
+        }
+      },
+      "color": {
+        "value": "#20f3be"
+      },
+      "shape": {
+        "type": "edge",
+        "stroke": {
+          "width": 0,
+          "color": "#000000"
+        },
+        "polygon": {
+          "nb_sides": 3
+        },
+        "image": {
+          "src": "img/github.svg",
+          "width": 100,
+          "height": 100
+        }
+      },
+      "opacity": {
+        "value": 0.5,
+        "random": false,
+        "anim": {
+          "enable": false,
+          "speed": 1,
+          "opacity_min": 0.1,
+          "sync": false
+        }
+      },
+      "size": {
+        "value": 3,
+        "random": true,
+        "anim": {
+          "enable": false,
+          "speed": 40,
+          "size_min": 0.1,
+          "sync": false
+        }
+      },
+      "line_linked": {
+        "enable": true,
+        "distance": 150,
+        "color": "#20f3be",
+        "opacity": 0,
+        "width": 0
+      },
+      "move": {
+        "enable": true,
+        "speed": 1,
+        "direction": "top-right",
+        "random": true,
+        "straight": true,
+        "out_mode": "out",
+        "bounce": false,
+        "attract": {
+          "enable": false,
+          "rotateX": 8818.766334760376,
+          "rotateY": 8417.913319543995
+        }
+      }
+    },
+    "interactivity": {
+      "detect_on": "window",
+      "events": {
+        "onhover": {
+          "enable": true,
+          "mode": "bubble"
+        },
+        "onclick": {
+          "enable": false,
+          "mode": "push"
+        },
+        "resize": true
+      },
+      "modes": {
+        "grab": {
+          "distance": 400,
+          "line_linked": {
+            "opacity": 1
+          }
+        },
+        "bubble": {
+          "distance": 146.16558937213767,
+          "size": 2,
+          "duration": 2.030077630168579,
+          "opacity": 0.37353428395101856,
+          "speed": 3
+        },
+        "repulse": {
+          "distance": 100,
+          "duration": 0.4
+        },
+        "push": {
+          "particles_nb": 4
+        },
+        "remove": {
+          "particles_nb": 2
+        }
+      }
+    },
+    "retina_detect": true
+  }
 
 export class Dashboard extends React.Component {
 
@@ -61,10 +173,11 @@ export class Dashboard extends React.Component {
         if(this.props.checkProgress) {
             return (
                 <div className="dashboard">
+                    <Particles className="particles" params={particleOptions}/>
                     <div className="progress-container">
                         <p className="progress-message">Here's how well you know your capitals, {this.props.username}</p>
                         <div className="progress-box">
-                            {stats.map((stat, index) => <span key={index}>{stat.answer}: {stat.numberOfSuccesses}</span>).sort()}
+                            {stats.map((stat, index) => <span className="progress-object" key={index}>{stat.answer}: {stat.numberOfSuccesses}</span>).sort()}
                         </div>
                         <button className="goback-button" onClick={() => this.props.dispatch(getProgress())}>Go back</button>
                     </div>
@@ -76,6 +189,7 @@ export class Dashboard extends React.Component {
                     {/* <div className="dashboard-username">
                         {username}
                     </div> */}
+                    <Particles className="particles" params={particleOptions}/>
                     <div className="dashboard-name">Hello, {this.props.name}!</div>
                     <h3 className="what-country-q">Which country is this capital city from?</h3>
                     <section className="question-area">
